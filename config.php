120de64ec18ef1ccac029f353d20c013489c436a
Martin Maly <maly@maly.cz>
2009-11-26 21:45:43 +0100

    First commit with templates

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * New file
+ *
+ * @version 1.0
+ * @author martin
+ * @copyright (C) 2009 martin
+ */
+
+$template = 'wall';
+$phrase = '#listopad OR #89 OR #1989';
+$phrase = '#20 OR #1989 OR #1711 OR listopad OR listopadu OR #velvetrevolution OR "velvet revolution" -clinic';
+$phrase = '#apple';
+$delicious = '816e65ff9638e4cdd3d0d3dae5b287d0';
+$language = ''; //default is :cz:
+//http://delicious.com/url/816e65ff9638e4cdd3d0d3dae5b287d0?show=all
+//http://delicious.com/save?url=http%3A%2F%2Fwww.mechatweet.com%2Fwall%2Flistopad%2F&hash=4308565238b8a6da5107bb7d8fd6f277&key=Ck4mC9Ivb0WOswCd2qKHyosHCa8-&edit=yes&jump=%2Fzdrojak&on_tags=
+$cfg = array(
+	'retweeturl' => 'http://www.mechatweet.com/wall/listopad',
+	'canonical' => 'http://www.mechatweet.com/wall/listopad',
+	'title' => 'Listopad 89 na Twitteru',
+	'theme' => 'Listopad 1989'
+	);
+
+?>
---
+++ templates/basic/js/application.js
@@ -0,0 +1,48 @@
+var effects=false;
+var timestamp='';
+function reload(){
+  $('#mt-loader').fadeIn('slow');
+  $.post('./update.php',
+    {'timestamp':timestamp},
+	
+    function(data){
+      $('#mt-loader').fadeOut('slow');
+      if(data){
+        $('.mt-marked').removeClass('mt-marked');
+        $('.mt-wall').prepend(data);
+        if(effects){
+          $('.mt-updated').addClass('mt-marked').children().not('a,img').fadeOut().fadeIn().fadeOut().fadeIn().fadeOut().fadeIn();
+        }else{
+          effects=true;
+        }
+        $('.mt-updated>a>img').hover(function(){
+          $(this).css({'z-index':10});
+          $(this).stop().animate({top:-12,left:-40,width:48,height:48},100);
+        }, function(){
+          $(this).css({'z-index':0});
+          $(this).stop().animate({top:0,left:-26,width:24,height:24},100);
+        });
+        $('.mt-updated').removeClass('mt-updated');
+        timestamp=/<!-- (\d*) -->/.exec(data)[1];
+		//removing
+		while($('.mt-item').length > 100) { $('.mt-item:last').remove();}
+      }
+      setTimeout(reload,10000);
+    }
+	
+  );
+};
+
+$(function(){
+  reload();
+  $.getJSON('./proxy.php?url=tweetmeme',
+    function(json){
+      $('#mt-twitter').text(json.story.url_count).parent().parent().show();
+    }
+  );
+  $.getJSON('./proxy.php?url=delicious',
+    function(json){
+      $('#mt-delicious').text(json[0]['total_posts']).parent().parent().show();
+    }
+  );
+});

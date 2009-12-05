3b997ae1c5869c69189870a51148fa8ea044f678
Martin Maly <maly@maly.cz>
2009-12-05 20:47:09 +0100

    Clean code

--- config.php
+++ config.php
@@ -16,16 +16,21 @@
  * @package    Wall
  */
 
-
+// Select template
 $template = 'wall';
-$phrase = '#listopad OR #89 OR #1989';
-$phrase = '#20 OR #1989 OR #1711 OR listopad OR listopadu OR #velvetrevolution OR "velvet revolution" -clinic';
-$phrase = 'vanoce';
-$delicious = 'e50114805d262774e798ede43d061c26';
-$language = ''; //default is :cz:
-//http://delicious.com/url/816e65ff9638e4cdd3d0d3dae5b287d0?show=all
 
-//http://delicious.com/save?url=http%3A%2F%2Fwww.mechatweet.com%2Fwall%2Flistopad%2F&hash=4308565238b8a6da5107bb7d8fd6f277&key=Ck4mC9Ivb0WOswCd2qKHyosHCa8-&edit=yes&jump=%2Fzdrojak&on_tags=
+//Enter search phrase 
+// $phrase = '#listopad OR #89 OR #1989';
+// $phrase = '#20 OR #1989 OR #1711 OR listopad OR listopadu OR #velvetrevolution OR "velvet revolution" -clinic';
+// $phrase = 'vanoce';
+$phrase = '#xmas';
+
+//Enter Delicious hashtag
+$delicious = 'e50114805d262774e798ede43d061c26';
+
+//Enter default language
+$language = ''; //default is :cz:
+
 $cfg = array(
 	'retweeturl' => 'http://www.mechatweet.com/wall/vanoce',
 	'canonical' => 'http://www.mechatweet.com/wall/vanoce',
--- proxy.php
+++ proxy.php
@@ -26,6 +26,4 @@ $url = '';
 if ($page == 'tweetmeme') $url = 'http://api.tweetmeme.com/url_info.json?url=' . $cfg['canonical'];
 if ($page == 'delicious') $url = 'http://feeds.delicious.com/v2/json/urlinfo/' . $delicious;
 
-if (!$url) return;
-
-readfile($url);
+if ($url) readfile($url);
